50cf28ee863ff8daff50318668393981015bbd49
Evaldas <vievaldas@gmail.com>
2020-10-06 17:37:53 +0300

    UserRegisterModel AuthController defined

---
+++ Support_Your_Locals/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Support_Your_Locals.Models;
+
+namespace Support_Your_Locals.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public ViewResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult SignUp(UserRegisterModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Index", user);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        public ViewResult SignIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Support_Your_Locals/Models/UserRegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Support_Your_Locals.Models
+{
+    public class UserRegisterModel
+    {
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your surname")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Please enter your birth date")]
+        public string BirthDate { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        public string Email { get; set; }
+
+    }
+}
