5b5370d1092206412fa5a3d3863e6f1e66b6f5cc
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2020-10-10 18:08:43 +0300

    Add timesheet to post, WIP JS for dynamic working hours

--- Support_Your_Locals/wwwroot/js/site.js
+++ Support_Your_Locals/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(() => {
+   const dayCheckBoxes = $('.day-check-box');
+   
+   $.each(dayCheckBoxes, (id, el) => {
+       $(el).on('change', (event) => {
+           if(event.target.checked) {
+               
+           }
+       });
+   });
+});
