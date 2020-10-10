12451c10c6b2c58d654a12a8046b06c59ec9aa5d
Valdas Vabolis <thisisvaldasvabolis@gmail.com>
2020-10-10 13:14:11 +0300

    Add Posts controller, Post#New view, remove old Add_advertise

---
+++ Support_Your_Locals/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Support_Your_Locals.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        
+        public IActionResult New()
+        {
+            return View();
+        }
+        
+        
+    }
+}
