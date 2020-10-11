c22a00128676f3a86ba21d2cf3327c1ed6221902
Evaldas <vievaldas@gmail.com>
2020-10-11 20:03:17 +0300

    Basic css

---
+++ Support_Your_Locals/Controllers/BusinessController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Support_Your_Locals.Controllers
+{
+    public class BusinessController : Controller
+    {
+
+        public ViewResult Index() => View();
+    }
+}
