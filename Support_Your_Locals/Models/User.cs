ae6aafd41bff7870a18aaedd144093eafa86ea17
Evaldas <vievaldas@gmail.com>
2020-10-11 14:31:28 +0300

    User class

---
+++ Support_Your_Locals/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Support_Your_Locals.Models
+{
+    public class User
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        [Key]
+        public string Email { get; set; }
+    }
+}
