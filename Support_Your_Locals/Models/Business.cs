327127228609fbee6c717e7c6415db00f4c45383
Evaldas <vievaldas@gmail.com>
2020-10-11 15:51:41 +0300

    Add Business model

---
+++ Support_Your_Locals/Models/Business.cs
@@ -0,0 +1,10 @@
+﻿namespace Support_Your_Locals.Models
+{
+    public class Business
+    {
+        public string Product { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Header { get; set; }
+    }
+}
