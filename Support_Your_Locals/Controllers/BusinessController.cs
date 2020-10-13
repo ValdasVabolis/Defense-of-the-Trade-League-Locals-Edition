c2ee12efed7b634f6bbe7e106b89f81bead4d78a
Evaldas <vievaldas@gmail.com>
2020-10-13 20:37:36 +0300

    Main page connection to individual business page

--- Support_Your_Locals/Controllers/BusinessController.cs
+++ Support_Your_Locals/Controllers/BusinessController.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Support_Your_Locals.Models;
+using Support_Your_Locals.Models.Repositories;
 
 namespace Support_Your_Locals.Controllers
 {
     public class BusinessController : Controller
     {
 
-        public ViewResult Index() => View();
+        private IServiceRepository repository;
+
+        public BusinessController(IServiceRepository repo)
+        {
+            repository = repo;
+        }
+
+        [HttpPost]
+        public ViewResult Index(long businessId)
+        {
+            Business business = repository.Business.FirstOrDefault(b => b.BusinessID == businessId);
+            User user = repository.Users.FirstOrDefault(u => u.UserID == business.UserID);
+            IEnumerable<TimeSheet> timeSheets = repository.TimeSheets.Where(t => t.BusinessID == business.BusinessID);
+            UserBusinessTimeSheets userBusinessTimeSheets = new UserBusinessTimeSheets
+            {
+                User = user,
+                Business = business,
+                TimeSheets = timeSheets
+            };
+            return View(userBusinessTimeSheets);
+        }
     }
 }
---
+++ Support_Your_Locals/Models/UserBusinessTimeSheets.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Support_Your_Locals.Models
+{
+    public class UserBusinessTimeSheets
+    {
+
+        public User User { get; set; }
+        public Business Business { get; set; }
+        public IEnumerable<TimeSheet> TimeSheets { get; set; }
+    }
+}
